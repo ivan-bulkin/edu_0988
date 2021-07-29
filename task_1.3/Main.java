00e7a138ffbcf0bcc923aa23e0f472361a83c45f
ivan-bulkin <75111842+ivan-bulkin@users.noreply.github.com>
2021-07-29 10:38:10 +0300

    Create Main.java

---
+++ task_1.3/Main.java
@@ -0,0 +1,12 @@
+
+/* Объяви переменные a, b типа int и переменную s типа String.
+Сразу же в строке объявления присвой им любые разные значения. */
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+        String s = "Москва";
+        System.out.println(a + ", " + b + ", " + s);
+    }
+}
